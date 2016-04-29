4537e518b69b2de1753a8ad03025a3c8bb2d7897
Sharayu Nargolkar <sharayu.nargolkar@sap.com>
2016-04-29 14:47:31 -0700

    trial

--- server.js
+++ server.js
@@ -4,24 +4,13 @@ var amazonProductsRoute = require('./server/routes/amazon-products');
 var pollenLoaderRoute = require('./server/routes/pollen-loader');
 
 
-
-
-
-
-
-// var port = process.env.VCAP_APP_PORT || 3000;
-
-// app.listen(port, function(err){
-//    console.log(`app started and listening on port: ${port}`);
-// });
-
 var path = require('path');
 var express = require('express');
 var amazonProductsRoute = require('./server/routes/amazon-products');
 
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
  app.use(express.static(__dirname + '/'));
 
  app.get("/",function(req,res){
@@ -30,23 +19,6 @@ res.sendfile(__dirname+"index.html");
 
 app.use('/api', amazonProductsRoute);
 app.use('/pollen', pollenLoaderRoute);
-
-
-// ----------------------------------------------
- var GoogleMapsLoader = require('./server/routes/gMap-loader'); 
-
- var myElement = angular.element("#map-canvas");
- var myOptions = {
-   zoom: 3,
-   center: myLatlng
- };
- var GoogleMapsLoader = require('google-maps'); // only for common js environments 
- GoogleMapsLoader.load(function(google) {
- 	new google.maps.Map(myElement, myOptions);
- });
-
- app.use('/gMap', GoogleMapsLoader);
-// ----------------------------------------------
 
 
 app.listen(port, function() {
